d53db796be675c54277007eed31c10aded805646
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-09 21:03:23 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,2 @@
-def valid_move?
-end# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index) 
+end

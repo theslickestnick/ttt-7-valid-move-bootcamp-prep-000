f66ef3db90500040f1d99aec2fb77c91023a6dde
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-09 21:17:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,7 @@
 def valid_move?(board, index)
-  index.between?(0, 8) && !position_taken?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
 end
